4bdf53cdc06cb322b61b5fe298159e2d11ab24f1
vanilla120707 <131329871+vanilla120707@users.noreply.github.com>
2023-05-13 11:34:46 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(100)
+        basic.showIcon(IconNames.Surprised)
+    }
+    no = randint(0, 200)
+    basic.showNumber(no / 1)
+})
+let no = 0
+basic.showIcon(IconNames.Heart)
+music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
